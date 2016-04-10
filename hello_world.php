e93aa7946df9187fe440481d9bdb07a103febd25
Warren Nelson <warren.nelson@gmail.com>
2016-04-10 20:38:30 +1000

    Added files via upload

---
+++ hello_world.php
@@ -0,0 +1,12 @@
+<?php
+	/* **************************
+	File to test if PHP is correctly loading
+	************************** */ 
+?><html>
+	<head>
+		<title>Hello World Test</title>
+	</head>
+	<body>
+		<?php echo '<p>Hello World!!!</p>'; ?> 
+	</body>
+</html>
